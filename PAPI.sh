79053dab4e1a04fb7d614556905c7afa50940a0f
Elie Koivunen <33964123+Elie-Koivunen@users.noreply.github.com>
2022-12-07 22:39:54 +0200

    Update PAPI.sh

--- PAPI.sh
+++ PAPI.sh
@@ -28,3 +28,22 @@ curl -vk "https://192.168.255.12:8080/platform/10/cluster/nodes" -b 'isisessid=e
 
 curl -vk "https://192.168.255.12:8080/platform/5/license/licenses" -b 'isisessid=e7a849b5-77a7-411d-b712-fdcae8d58669' -H 'X-CSRF-Token: 544eebf1-6ad7-4260-9656-40c152801308' --referer https://192.168.255.12:8080
 
+==========================================
+#version 2
+
+#auth.json file content
+
+{
+"username": "root",
+"password": "a",
+"services": ["platform","namespace"]
+}
+
+
+myapiip=$(echo 192.168.255.11);echo $myapiip
+curl -k --header "Content-Type: application/json" -c pipari.txt -X POST -d @auth.json https://$myapiip:8080/session/1/session
+mycsrf=$(cat pipari.txt|egrep -i isicsrf|awk '{print "X-CSRF-Token:",$7;}');echo $mycsrf
+myisisessid=$(cat pipari.txt|egrep -i isisessid|awk '{print "isisessid="$7;}');echo $myisisessid
+myapisearch=$(echo platform/3/statistics/summary/system);echo $myapisearch
+echo ;echo Target API: $myapiip;echo Query: $myapisearch;echo CSRF token: $mycsrf;echo Session ID: $myisisessid;echo
+curl -vk "https://$myapiip:8080/$myapisearch" -b "$myisisessid" -H "$mycsrf" --referer "https://$myapiip:8080"
