8d350ea15428500fce6b0a0357f86f72b75a72ea
Elie Koivunen <33964123+Elie-Koivunen@users.noreply.github.com>
2024-03-11 16:39:24 +0200

    Update IIQ-enable-account.sh

--- IIQ-enable-account.sh
+++ IIQ-enable-account.sh
@@ -1,2 +1,8 @@
-isi auth users modify insightiq --enabled=true --password=Foobar123 --unlock 
+isi auth users modify insightiq --enabled=true --password=Password123! --unlock 
 isi job jobs start FSAnalyze --policy=MEDIUM --priority=4
+
+
+onefs +9.5
+
+isi auth users reset-password insightiq --zone=system --provider=lsa-file-provider:System
+isi auth users modify insightiq --zone=system --provider=lsa-file-provider:System --enabled=true
